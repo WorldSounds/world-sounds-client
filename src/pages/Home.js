bf72794fa41f11de53430d3329518e1adfd8b910
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-02-21 01:32:15 +0700

    delete unnecessary files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 import { 
   LoginModal,
   Navbar,
   RegisterModal 
 } from '../components';
-
+import ZoomableBubbleChart from '../components/ZoomableBubbleChart'
+import "../../node_modules/react-bubble-chart/src/style.css";
+import genres from '../Assets/JSON/chartData.json'
 
 import { useHistory } from 'react-router-dom'
 
 export default function Home() {
-
 
   return(
     <div className="home" style={{ backgroundColor: 'rgba(14, 18, 66, 1)' }}>
       <Navbar />
       <div>
         <div className="circle">
-          
+          <ZoomableBubbleChart data={genres} />
         </div>
       </div>
     </div>
